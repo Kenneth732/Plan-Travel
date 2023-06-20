83efb5e099b7e6227f5b8445142c734bc5d0e349
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-20 09:24:25 +0300

    add selector and add submit on form

--- static/js/script.js
+++ static/js/script.js
@@ -13,4 +13,24 @@ window.addEventListener('load', () => {
     }
 });
 
+// Select the form element and add a submit event listener
+document.querySelector('#form').addEventListener('submit', (e) => {
+    e.preventDefault();
 
+    // Get the values entered in the trip input fields
+    let destinationInput = document.querySelector('#destinationInput');
+    let startDateInput = document.querySelector('#startDateInput');
+    let endDateInput = document.querySelector('#endDateInput');
+
+    let destination = destinationInput.value;
+    let startDate = startDateInput.value;
+    let endDate = endDateInput.value;
+
+    // Validate the input
+    if (destination.trim() === '' || startDate === '' || endDate === '') {
+        // Show an error message or provide visual feedback to the user
+        alert('Please fill in all fields.');
+        return; // Abort adding the trip
+    }
+
+})
